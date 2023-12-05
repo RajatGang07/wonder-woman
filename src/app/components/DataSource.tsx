0496a3b94a7e04a29e69bbd4476ad92cb96c1611
Rajat Gang <rajatgang@Rajats-MacBook-Pro.local>
2023-12-06 00:04:16 +0530

    fb login

--- src/app/components/DataSource.tsx
+++ src/app/components/DataSource.tsx
@@ -1,4 +1,5 @@
 "use client";
+import axios from "axios";
 import React, { useState } from "react";
 import Select from "react-select";
 
@@ -9,13 +10,25 @@ export default function DataSource() {
     { value: "facebook", label: "Facebook" },
     { value: "youtube", label: "Youtube" },
   ];
+
+  const handleSelectedOption = async (selected: any) => {
+    setSelectedOption(selected);
+    
+    const response: any = await fetch(
+      `https://www.facebook.com/v18.0/dialog/oauth?client_id=2584462238376302&redirect_uri=https://wakanda-forever.azurewebsites.net&scope=ads_read&response_type=code`,
+    );
+
+    console.log('response', response)
+  };
+
+  
   return (
     <div className="flex  items-center ml-[300px] pb-8 gap-4">
       <label>Select a Data Source</label>
 
       <Select
         defaultValue={selectedOption}
-        onChange={setSelectedOption}
+        onChange={handleSelectedOption}
         options={options}
       />
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
       <body className={inter.className}>
         <Providers>
           {children}
