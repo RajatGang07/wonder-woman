a0b286b50f72a49e4ac8126fce8ac978836e5f44
Rajat Gang <rajatgang@Rajats-MacBook-Pro.local>
2024-01-02 22:32:50 +0530

    changes

--- src/app/middleware.tsx
+++ src/app/middleware.tsx
@@ -11,7 +11,7 @@ export function middleware() {
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        'X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
 
     return res
