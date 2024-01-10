a8ffa805d22413bb4daefe4d55b420df505e61ad
Rajat Gang <rajatgang@Rajats-MacBook-Pro.local>
2024-01-10 21:35:16 +0530

    change file ext

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -4,8 +4,9 @@ import FacebookProvider from "next-auth/providers/facebook";
 export const options = {
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      idToken: true,
+      clientId: '1361038827821051',
+      clientSecret: 'cad14b5e85a97174de6eedb7d1912589',
       scope: 'email,ads_read,public_profile,read_insights,pages_read_engagement,business_management',
     }),
 
