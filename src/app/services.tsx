ddc1755db5db9b63a57e4bc5fa45686b63f102e2
Rajat Gang <rajatgang@Rajats-MacBook-Pro.local>
2024-01-22 21:15:58 +0530

    change service

--- src/app/services.tsx
+++ src/app/services.tsx
@@ -1,5 +1,5 @@
 export const DOMAIN_URL = {
   // localhost: 'http://localhost:8080',
-  // prod: 'https://thunder-bird.azurewebsites.net'
-  prod: "http://localhost:8080",
+  prod: 'https://thunder-bird.azurewebsites.net'
+  // prod: "http://localhost:8080",
 };
